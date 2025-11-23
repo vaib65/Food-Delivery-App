94f79c254d489d8fd866fc1e4e7fc120f3688692
vaib65 <vhadke.02@gmail.com>
2025-11-23 10:47:14 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders/Orders'
 import { ToastContainer } from "react-toastify";
 
 const App = () => {
-  const url="http://localhost:4000"
+  const url="https://food-delivery-app-backend-hnmy.onrender.com"
 
   return (
     <div>
